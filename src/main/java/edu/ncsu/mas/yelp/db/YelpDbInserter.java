dbb3bad1fb483973c75f7b99c597a0043a4bd0e3
Pradeep Murukannaiah <pradeepmcl@gmail.com>
2015-04-11 18:31:50 -0400

    Code to insert to user table seems to work.

--- src/main/java/edu/ncsu/mas/yelp/db/YelpDbInserter.java
+++ src/main/java/edu/ncsu/mas/yelp/db/YelpDbInserter.java
@@ -17,13 +17,13 @@ import java.text.SimpleDateFormat;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class UserTableInserter implements AutoCloseable {
+public class YelpDbInserter implements AutoCloseable {
 
   private static final String mysqlDriver = "com.mysql.jdbc.Driver";
   private final String mDbUrl;
   private final Connection mConn;
 
-  public UserTableInserter(String dbUrl) throws InstantiationException, IllegalAccessException,
+  public YelpDbInserter(String dbUrl) throws InstantiationException, IllegalAccessException,
       ClassNotFoundException, SQLException {
     Class.forName(mysqlDriver).newInstance();
     mDbUrl = dbUrl;
@@ -37,19 +37,65 @@ public class UserTableInserter implements AutoCloseable {
     }
   }
 
-  private final String insertQuery = "INSERT INTO User(id_original_usr, name_usr, "
+  private final DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM");
+
+  private final String userInsertQuery = "INSERT INTO User(id_original_usr, name_usr, "
       + "yelping_since_usr, review_count_usr, fans_usr, type_usr, avg_stars_usr, votes_funny_usr, "
       + "votes_useful_usr, votes_cool_usr, compliments_profile_usr, compliments_cute_usr, "
       + "compliments_funny, compliments_plain_usr, compliments_writer_usr, compliments_note_usr, "
       + "compliments_photos_usr, compliments_hot_usr, compliments_cool_usr, compliments_more_usr) "
       + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-  private final DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM");
+  private String getInsertQuery(String tableName) {
+    tableName = tableName.toLowerCase();
+    switch (tableName) {
+    case "user":
+      return userInsertQuery;
+    default:
+      throw new IllegalArgumentException("Unknown table: " + tableName);
+    }
+  }
 
-  private void prepareStatment(PreparedStatement prpdStmt, JSONObject userOb) throws JSONException,
-      SQLException, ParseException {
-    prpdStmt.clearParameters();
+  private void setParameterValues(String tableName, PreparedStatement prpdStmt, JSONObject userOb)
+      throws JSONException, SQLException, ParseException {
+    tableName = tableName.toLowerCase();
+    switch (tableName) {
+    case "user":
+      setUserParameterValues(prpdStmt, userOb);
+      break;
+    default:
+      throw new IllegalArgumentException("Unknown table: " + tableName);
+    }
+  }
 
+  public void insert(String filename, String tableName) throws FileNotFoundException, IOException,
+      SQLException, JSONException, ParseException {
+    mConn.setAutoCommit(false);
+
+    String insertQuery = getInsertQuery(tableName);
+
+    try (BufferedReader br = new BufferedReader(new FileReader(filename));
+        PreparedStatement prpdStmt = mConn.prepareStatement(insertQuery)) {
+      // Each line is in json format (e.g., resources/user_sample.json)
+      int insertedSoFar = 0;
+      for (String line = br.readLine(); line != null; line = br.readLine()) {
+        JSONObject jsonOb = new JSONObject(line);
+        setParameterValues(tableName, prpdStmt, jsonOb);
+        prpdStmt.addBatch();
+        if (insertedSoFar++ % 20000 == 0) {
+          prpdStmt.executeBatch();
+          mConn.commit();
+          System.out.println("Inserted so far: " + insertedSoFar);
+        }
+      }
+      prpdStmt.executeBatch(); // Left overs
+      mConn.commit();
+      mConn.setAutoCommit(true);
+    }
+  }
+
+  private void setUserParameterValues(PreparedStatement prpdStmt, JSONObject userOb)
+      throws JSONException, SQLException, ParseException {
     prpdStmt.setString(1, userOb.getString("user_id"));
     prpdStmt.setString(2, userOb.getString("name"));
     prpdStmt.setDate(3, new Date(dateFormatter.parse(userOb.getString("yelping_since")).getTime()));
@@ -71,34 +117,12 @@ public class UserTableInserter implements AutoCloseable {
     JSONObject complimentsOb = userOb.getJSONObject("votes");
     String[] complimentTypes = { "profile", "cute", "funny", "plain", "writer", "note", "photos",
         "hot", "cool", "more" };
-    for (int i = 11; i <= 10; i++) {
-      if (complimentsOb.has(complimentTypes[i - 8])) {
-        prpdStmt.setInt(i, complimentsOb.getInt(complimentTypes[i - 8]));
+    for (int i = 11; i <= 20; i++) {
+      if (complimentsOb.has(complimentTypes[i - 11])) {
+        prpdStmt.setInt(i, complimentsOb.getInt(complimentTypes[i - 11]));
       } else {
         prpdStmt.setNull(i, Types.INTEGER);
       }
-    }
-  }
-
-  public void insert(String filename) throws FileNotFoundException, IOException, SQLException,
-      JSONException, ParseException {
-    mConn.setAutoCommit(false);
-    try (BufferedReader br = new BufferedReader(new FileReader(filename));
-        PreparedStatement prpdStmt = mConn.prepareStatement(insertQuery)) {
-      // Each line is in json format (e.g., resources/user_sample.json)
-      int insertedSoFar = 0;
-      for (String line = br.readLine(); line != null; line = br.readLine()) {
-        JSONObject jsonOb = new JSONObject(line);
-        prepareStatment(prpdStmt, jsonOb);
-        prpdStmt.addBatch();
-        if (++insertedSoFar % 10000 == 0) {
-          prpdStmt.executeUpdate();
-          mConn.commit();
-          System.out.println("Inserted so far: " + insertedSoFar);
-        }
-      }
-      prpdStmt.executeUpdate(); // Left overs
-      mConn.commit();
     }
   }
 
@@ -106,9 +130,10 @@ public class UserTableInserter implements AutoCloseable {
       ClassNotFoundException, SQLException, FileNotFoundException, IOException, JSONException,
       ParseException {
     String dbUrl = args[0];
-    String inFilename = args[1];
-    try (UserTableInserter inserter = new UserTableInserter(dbUrl)) {
-      inserter.insert(inFilename);
+    String tableName = args[1];
+    String inFilename = args[2];
+    try (YelpDbInserter inserter = new YelpDbInserter(dbUrl)) {
+      inserter.insert(inFilename, tableName);
     }
   }
 }
